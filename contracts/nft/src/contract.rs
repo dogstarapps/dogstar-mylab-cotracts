d905cadbc14d03439f99469909dd92461664455a
quiet-ocean <91833579+quiet-ocean@users.noreply.github.com>
2025-05-01 16:11:02 +0200

    add interfaces to get positions

--- contracts/nft/src/contract.rs
+++ contracts/nft/src/contract.rs
@@ -1,7 +1,7 @@
 //! This contract demonstrates a sample implementation of the Soroban token
 //! interface.
 
-use crate::actions::{burn, deck, fight, lending, stake, Deck, SidePosition};
+use crate::actions::{burn, deck, fight, lending, lending::{Lending, Borrowing}, stake, Deck, SidePosition};
 use crate::admin::{
     add_level, has_administrator, read_administrator, read_balance, read_config, read_state,
     update_level, write_administrator, write_balance, write_config, Balance, Config, State,
@@ -399,6 +399,10 @@ impl NFT {
     ) -> stake::Stake {
         stake::read_stake(env, fee_payer, category, token_id)
     }
+
+    pub fn read_stakes(env: Env) -> Vec<stake::Stake> {
+        stake::read_stakes(env)
+    }
 }
 
 // Fight
@@ -441,6 +445,10 @@ impl NFT {
     ) -> fight::Fight {
         fight::read_fight(env, fee_payer, category, token_id)
     }
+
+    pub fn read_fights(env: Env) -> Vec<fight::Fight> {
+        fight::read_fights(env)
+    }
 }
 
 // Lend & Borrow
@@ -479,6 +487,12 @@ impl NFT {
         token_id: TokenId,
     ) -> lending::Borrowing {
         lending::read_borrowing(env, player, category, token_id)
+    }
+    pub fn read_borrowings(env: Env) -> Vec<Borrowing> {
+        lending::read_borrowings(env)
+    }
+    pub fn read_lendings(env: Env) -> Vec<Lending> {
+        lending::read_lendings(env)
     }
 }
 
